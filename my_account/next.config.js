a63783b80e469623bf01576e31489408b6745d27
eundonghyeok <mephisto7191@naver.com>
2024-12-18 00:31:34 +0900

    초기 세팅 - emotion 설정

--- my_account/next.config.js
+++ my_account/next.config.js
@@ -1,6 +1,12 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  compiler: {
+    emotion: true,
+  },
 }
 
 module.exports = nextConfig
